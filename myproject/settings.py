2195f2d583b765a5392d394252a98de712db2d41
Pietro Spagnulo <pspagnulo@gmail.com>
2017-03-06 17:13:47 +0100

    settings.py changed after django-scheduler

--- myproject/settings.py
+++ myproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'learning',
     'bootstrap3',
+    'scedule',
 ]
 
 MIDDLEWARE = [
@@ -64,10 +65,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request',
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
